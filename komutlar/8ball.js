072ab28c7aee4736af88b8f5c50cce2dee9f7406
ebrarg <42240618+ebrarg@users.noreply.github.com>
2018-10-29 20:48:18 +0300

    Update 8ball.js

--- komutlar/8ball.js
+++ komutlar/8ball.js
@@ -1,24 +1,42 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
-const cevaplar = [
-    "evet",
-    "hayır",
-    "belki",
-    "olabilir",
-    "daha sonra tekrar sor",
-    "imkansız"
-];
+exports.run = (client, message, args, tools) => {
+if(!args[0]) {
+  const errEmbed = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setAuthor('ERROR')
+  .setTitle(':exclamation: Usage: **ks!8ball (question)**');
+  message.channel.send({embed: errEmbed})
+  return;
+}
+var sayings = ["Kesinlikle",
+										"Eminimki öyle.",
+										"Şüphesiz.",
+										"Evet, kesinlikle",
+										"Bana güvenebilirsin",
+										"Görünüşe göre evet",
+										"Büyük ihtimalle",
+										"İyi görünüyor",
+										"Evet",
+										"Evet gibi görünüyor",
+										"Soruyu tekrar sor",
+										"Sonra sor",
+										"Sana söylemesem daha iyi",
+										"Tahmin edemiyorum",
+										"Konsantre ol ve tekrar sor",
+										"Bana güvenme",
+										"Benim cevabım hayır",
+										"Kaynaklarım hayır diyor",
+										"Görünüşe göre hayır.",
+										"İmkansız."];
 
-exports.run = function(client, message, args) {
-    var soru = args.join(' ');
-
-    var cevap = cevaplar[Math.floor(Math.random() * cevaplar.length)];
-
-    if(!soru) return message.reply('Bir soru belirt. **Doğru Kullanim**: !8ball <soru>')
-    else message.channel.send(cevap)
-
-};  
-
+			var result = Math.floor((Math.random() * sayings.length) + 0);
+      const ballEmb = new Discord.RichEmbed()
+      .setColor(0x00FFFF)
+      .setAuthor('8ball', 'https://findicons.com/files/icons/1700/2d/512/8_ball.png')
+      .addField(args, sayings[result]);
+			message.channel.send({embed: ballEmb})
+}
 exports.conf = {
   enabled: true, 
   guildOnly: true, 
